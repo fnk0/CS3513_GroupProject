62c1a13b2eddb433f85bb769646006f3d5b9dad7
marcus <marcusandreog@gmail.com>
2016-11-11 15:12:05 -0600

    changes

---
+++ models.js
@@ -0,0 +1,23 @@
+//takes the dataset, and function order,
+//returns a list of coefficents
+
+
+var polynomial = function (dataset, order) {
+    //loop over all points and calculate the all the sums.
+
+    //plug into the matrix
+
+    //row reduce.
+    var result = []; //return a list of coefficients
+
+    var m = [];
+
+    for(var i =0; i < dataset.length; i++) {
+
+    }
+
+    return result
+};
+
+
+module.exports = polynomial;
