c32be7c3c155df91d5e448d19f4b7cfe9189f7eb
Alex <o0o.moro.o0o@gmail.com>
2016-11-11 15:38:38 -0600

    initialize xsums and ysums to 0

--- models.js
+++ models.js
@@ -3,20 +3,24 @@
 
 
 var polynomial = function (dataset, order) {
-    //loop over all points and calculate the all the sums.
-    var xsums = [];
-    var ysums = [];
-    for (var i = dataset.length; i > 0; i--) {
-        var point = dataset[i];
-        if (point.co2 != "") {
-		for (var j = 0; j < 2 * order; j++){
-			xsums[j] = xsums[j] + Math.pow(point.year, j);
+	//loop over all points and calculate the all the sums.
+	var xsums = [];
+	var ysums = [];
+	//reset xsums and ysums to 0
+	for (var j = 0; j < 2 * order; j++){ xsums[j]=0 }
+	for (var j = 0; j < order+1; j++){ ysums[j]=0 }
+	//and calculate the sums
+	for (var i = dataset.length; i > 0; i--) {
+		var point = dataset[i];
+		if (point.co2 != "") {
+			for (var j = 0; j < 2 * order; j++){
+				xsums[j] = xsums[j] + Math.pow(point.year, j);
+			}
+			for (var j=0; j < order+1; j++){
+				ysums[j] = ysums[j] + point.co2*(Math.pow(point.year, j));
+			}
 		}
-		for (var j=0; j < order+1; j++){
-			ysums[j] = ysums[j] + point.co2*(Math.pow(point.year, j));
-		}
-        }
-    }
+	}
 
 
     //plug into the matrix
